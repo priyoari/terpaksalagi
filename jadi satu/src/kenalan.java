df75b201caefaa90126f6f7a5436f342f2696176
riyoari <priyo.ari.wibowo@gmail.com>
2015-05-03 21:40:43 +0700

    Initial commit

--- jadi satu/src/kenalan.java
+++ jadi satu/src/kenalan.java
@@ -1,10 +1,528 @@
 public class kenalan {
-    // identitas ruang
-    String saya;
-    // kondisi ruang
-    // jumlah,kondisi dan posisi
-    // lingkungan
-    // kebersihan
-    // kenyamanan
-    // keamanan
+// identitas ruang
+    private String namaRuang;
+    private String lokasiRuang;
+    private String programStudi;
+    
+// kondisi ruang
+    private int Panjang;
+    private int Lebar;
+    private int Luas;
+    private String Bentuk;
+    private int Jkursi;
+    private int Rasio;
+    private int Jpintu;
+    private int Jjendela;
+    private String Analisisruang;
+    
+// jumlah,kondisi dan posisi
+    private int Kontak;
+    private String KondisiKontak;
+    private String PosisiKontak;
+    
+    private int KabelLCD;
+    private String KondisiKabelLCD;
+    private String PosisiKabelLCD;
+    
+    private int Lampu;
+    private String KondisiLampu;
+    private String PosisiLampu;
+    
+    private int Kipas;
+    private String KondisiKipas;
+    private String PosisiKipas;
+    
+    private int AC;
+    private String KondisiAC;
+    private String PosisiAC;
+    
+    private int SSID;
+    private int Bandwidth;
+    
+    private int CCTV;
+    private String KondisiCCTV;
+    private String PosisiCCTV;
+
+// lingkungan
+    private int Klantai;
+    private int Kdinding;
+    private int Katap;
+    private int Kpintu;
+    private int Kjendela;
+    private String Analisis;
+    
+// kebersihan
+    private int sirkulasi;
+    private int pencahayaan;
+    private int lembab;
+    private int suhu;
+    
+// kenyamanan
+    private int Tkebisingan;
+    private int Tbau;
+    private int Tkebocoran;
+    private int Tkerusakan;
+    private int Tkeausan;
+    
+// keamanan
+    private String kokoh;
+    private String kunci;
+    private String jendela;
+    private String kondisi;
+
+    public String getNamaRuang() {
+        return namaRuang;
+    }
+
+    public void setNamaRuang(String namaRuang) {
+        this.namaRuang = namaRuang;
+    }
+
+    public String getLokasiRuang() {
+        return lokasiRuang;
+    }
+
+    public void setLokasiRuang(String lokasiRuang) {
+        this.lokasiRuang = lokasiRuang;
+    }
+
+    public String getProgramStudi() {
+        return programStudi;
+    }
+
+    public void setProgramStudi(String programStudi) {
+        this.programStudi = programStudi;
+    }
+
+    public int getPanjang() {
+        return Panjang;
+    }
+
+    public void setPanjang(int Panjang) {
+        this.Panjang = Panjang;
+    }
+
+    public int getLebar() {
+        return Lebar;
+    }
+
+    public void setLebar(int Lebar) {
+        this.Lebar = Lebar;
+    }
+
+    public int getLuas() {
+        return Luas;
+    }
+
+    public void setLuas(int Luas) {
+        this.Luas = Luas;
+    }
+
+    public String getBentuk() {
+        return Bentuk;
+    }
+
+    public void setBentuk(String Bentuk) {
+        this.Bentuk = Bentuk;
+    }
+
+    public int getJkursi() {
+        return Jkursi;
+    }
+
+    public void setJkursi(int Jkursi) {
+        this.Jkursi = Jkursi;
+    }
+
+    public int getRasio() {
+        return Rasio;
+    }
+
+    public void setRasio(int Rasio) {
+        this.Rasio = Rasio;
+    }
+
+    public int getJpintu() {
+        return Jpintu;
+    }
+
+    public void setJpintu(int Jpintu) {
+        this.Jpintu = Jpintu;
+    }
+
+    public int getJjendela() {
+        return Jjendela;
+    }
+
+    public void setJjendela(int Jjendela) {
+        this.Jjendela = Jjendela;
+    }
+
+    public String getAnalisisruang() {
+        return Analisisruang;
+    }
+
+    public void setAnalisisruang(String Analisisruang) {
+        this.Analisisruang = Analisisruang;
+    }
+
+    public int getIntKontak() {
+        return Kontak;
+    }
+
+    public void setIntKontak(int IntKontak) {
+        this.Kontak = IntKontak;
+    }
+
+    public String getKondisiKontak() {
+        return KondisiKontak;
+    }
+
+    public void setKondisiKontak(String KondisiKontak) {
+        this.KondisiKontak = KondisiKontak;
+    }
+
+    public String getPosisiKontak() {
+        return PosisiKontak;
+    }
+
+    public void setPosisiKontak(String PosisiKontak) {
+        this.PosisiKontak = PosisiKontak;
+    }
+
+    public int getIntKabelLCD() {
+        return KabelLCD;
+    }
+
+    public void setIntKabelLCD(int IntKabelLCD) {
+        this.KabelLCD = IntKabelLCD;
+    }
+
+    public String getKondisiKabelLCD() {
+        return KondisiKabelLCD;
+    }
+
+    public void setKondisiKabelLCD(String KondisiKabelLCD) {
+        this.KondisiKabelLCD = KondisiKabelLCD;
+    }
+
+    public String getPosisiKabelLCD() {
+        return PosisiKabelLCD;
+    }
+
+    public void setPosisiKabelLCD(String PosisiKabelLCD) {
+        this.PosisiKabelLCD = PosisiKabelLCD;
+    }
+
+    public int getIntLampu() {
+        return Lampu;
+    }
+
+    public void setIntLampu(int IntLampu) {
+        this.Lampu = IntLampu;
+    }
+
+    public String getKondisiLampu() {
+        return KondisiLampu;
+    }
+
+    public void setKondisiLampu(String KondisiLampu) {
+        this.KondisiLampu = KondisiLampu;
+    }
+
+    public String getPosisiLampu() {
+        return PosisiLampu;
+    }
+
+    public void setPosisiLampu(String PosisiLampu) {
+        this.PosisiLampu = PosisiLampu;
+    }
+
+    public int getIntKipas() {
+        return Kipas;
+    }
+
+    public void setIntKipas(int IntKipas) {
+        this.Kipas = IntKipas;
+    }
+
+    public String getKondisiKipas() {
+        return KondisiKipas;
+    }
+
+    public void setKondisiKipas(String KondisiKipas) {
+        this.KondisiKipas = KondisiKipas;
+    }
+
+    public String getPosisiKipas() {
+        return PosisiKipas;
+    }
+
+    public void setPosisiKipas(String PosisiKipas) {
+        this.PosisiKipas = PosisiKipas;
+    }
+
+    public int getIntAC() {
+        return AC;
+    }
+
+    public void setIntAC(int IntAC) {
+        this.AC = IntAC;
+    }
+
+    public String getKondisiAC() {
+        return KondisiAC;
+    }
+
+    public void setKondisiAC(String KondisiAC) {
+        this.KondisiAC = KondisiAC;
+    }
+
+    public String getPosisiAC() {
+        return PosisiAC;
+    }
+
+    public void setPosisiAC(String PosisiAC) {
+        this.PosisiAC = PosisiAC;
+    }
+
+    public int getSSID() {
+        return SSID;
+    }
+
+    public void setSSID(int SSID) {
+        this.SSID = SSID;
+    }
+
+    public int getBandwidth() {
+        return Bandwidth;
+    }
+
+    public void setBandwidth(int Bandwidth) {
+        this.Bandwidth = Bandwidth;
+    }
+
+    public int getIntCCTV() {
+        return CCTV;
+    }
+
+    public void setIntCCTV(int IntCCTV) {
+        this.CCTV = IntCCTV;
+    }
+
+    public String getKondisiCCTV() {
+        return KondisiCCTV;
+    }
+
+    public void setKondisiCCTV(String KondisiCCTV) {
+        this.KondisiCCTV = KondisiCCTV;
+    }
+
+    public String getPosisiCCTV() {
+        return PosisiCCTV;
+    }
+
+    public void setPosisiCCTV(String PosisiCCTV) {
+        this.PosisiCCTV = PosisiCCTV;
+    }
+
+    public int getKlantai() {
+        return Klantai;
+    }
+
+    public void setKlantai(int Klantai) {
+        this.Klantai = Klantai;
+    }
+
+    public int getKdinding() {
+        return Kdinding;
+    }
+
+    public void setKdinding(int Kdinding) {
+        this.Kdinding = Kdinding;
+    }
+
+    public int getKatap() {
+        return Katap;
+    }
+
+    public void setKatap(int Katap) {
+        this.Katap = Katap;
+    }
+
+    public int getKpintu() {
+        return Kpintu;
+    }
+
+    public void setKpintu(int Kpintu) {
+        this.Kpintu = Kpintu;
+    }
+
+    public int getKjendela() {
+        return Kjendela;
+    }
+
+    public void setKjendela(int Kjendela) {
+        this.Kjendela = Kjendela;
+    }
+
+    public String getAnalisis() {
+        return Analisis;
+    }
+
+    public void setAnalisis(String Analisis) {
+        this.Analisis = Analisis;
+    }
+
+    public int getSirkulasi() {
+        return sirkulasi;
+    }
+
+    public void setSirkulasi(int sirkulasi) {
+        this.sirkulasi = sirkulasi;
+    }
+
+    public int getPencahayaan() {
+        return pencahayaan;
+    }
+
+    public void setPencahayaan(int pencahayaan) {
+        this.pencahayaan = pencahayaan;
+    }
+
+    public int getLembab() {
+        return lembab;
+    }
+
+    public void setLembab(int lembab) {
+        this.lembab = lembab;
+    }
+
+    public int getSuhu() {
+        return suhu;
+    }
+
+    public void setSuhu(int suhu) {
+        this.suhu = suhu;
+    }
+
+    public int getTkebisingan() {
+        return Tkebisingan;
+    }
+
+    public void setTkebisingan(int Tkebisingan) {
+        this.Tkebisingan = Tkebisingan;
+    }
+
+    public int getTbau() {
+        return Tbau;
+    }
+
+    public void setTbau(int Tbau) {
+        this.Tbau = Tbau;
+    }
+
+    public int getTkebocoran() {
+        return Tkebocoran;
+    }
+
+    public void setTkebocoran(int Tkebocoran) {
+        this.Tkebocoran = Tkebocoran;
+    }
+
+    public int getTkerusakan() {
+        return Tkerusakan;
+    }
+
+    public void setTkerusakan(int Tkerusakan) {
+        this.Tkerusakan = Tkerusakan;
+    }
+
+    public int getTkeausan() {
+        return Tkeausan;
+    }
+
+    public void setTkeausan(int Tkeausan) {
+        this.Tkeausan = Tkeausan;
+    }
+
+    public String getKokoh() {
+        return kokoh;
+    }
+
+    public void setKokoh(String kokoh) {
+        this.kokoh = kokoh;
+    }
+
+    public String getKunci() {
+        return kunci;
+    }
+
+    public void setKunci(String kunci) {
+        this.kunci = kunci;
+    }
+
+    public String getJendela() {
+        return jendela;
+    }
+
+    public void setJendela(String jendela) {
+        this.jendela = jendela;
+    }
+
+    public String getKondisi() {
+        return kondisi;
+    }
+
+    public void setKondisi(String kondisi) {
+        this.kondisi = kondisi;
+    }
+
+    public int getKontak() {
+        return Kontak;
+    }
+
+    public void setKontak(int Kontak) {
+        this.Kontak = Kontak;
+    }
+
+    public int getKabelLCD() {
+        return KabelLCD;
+    }
+
+    public void setKabelLCD(int KabelLCD) {
+        this.KabelLCD = KabelLCD;
+    }
+
+    public int getLampu() {
+        return Lampu;
+    }
+
+    public void setLampu(int Lampu) {
+        this.Lampu = Lampu;
+    }
+
+    public int getKipas() {
+        return Kipas;
+    }
+
+    public void setKipas(int Kipas) {
+        this.Kipas = Kipas;
+    }
+
+    public int getAC() {
+        return AC;
+    }
+
+    public void setAC(int AC) {
+        this.AC = AC;
+    }
+
+    public int getCCTV() {
+        return CCTV;
+    }
+
+    public void setCCTV(int CCTV) {
+        this.CCTV = CCTV;
+    }
 }
--- jadi satu/src/kontrol.java
+++ jadi satu/src/kontrol.java
@@ -1,9 +1,123 @@
+
+import java.util.Scanner;
+
 public class kontrol {
+    Scanner scan = new Scanner(System.in);
+    kenalan kenal = new kenalan();
+    
+    void input(){
     // identitas ruang
+    System.out.println("Masukan nama ruang: ");
+    kenal.setNamaRuang(scan.next());
+    System.out.println("Masukan lokasi ruang: ");
+    kenal.setLokasiRuang(scan.next());
+    System.out.println("Pilih Program Studi/Fakultas");
+    kenal.setProgramStudi(scan.next());
+    
     // kondisi ruang
+    System.out.println("Masukkan panjang ruang  : ");
+    kenal.setPanjang(scan.nextInt());
+        
+    System.out.println("Masukkan lebar ruang    : ");
+    kenal.setLebar(scan.nextInt());
+            
+    System.out.println("Masukkan jumlah pintu   : ");
+    kenal.setJpintu(scan.nextInt());
+        
+    System.out.println("Masukkan jumlah jendela : ");
+    kenal.setJjendela(scan.nextInt());
+        
     // jumlah,kondisi dan posisi
+    System.out.println("Jumlah stop kontak: ");
+    kenal.setKontak(scan.nextInt());
+    System.out.print("Kondisi stop kontak baik (ya/tidak): ");
+    kenal.setKondisi(scan.next());
+    System.out.print("Posisi stop kontak dekat dosen & dipojok (ya/tidak): ");
+    kenal.setPosisiKontak(scan.next());
+    
     // lingkungan
+    System.out.println("Kondisi Lantai  : ");
+    System.out.println("1. Bersih");
+    System.out.println("2. Kotor");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setKlantai(scan.nextInt());
+            
+    System.out.println("Kondisi Dinding : ");
+    System.out.println("1. Bersih");
+    System.out.println("2. Kotor");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setKdinding(scan.nextInt());
+            
+    System.out.println("Kondisi Atap    : ");
+    System.out.println("1. Bersih");
+    System.out.println("2. Kotor");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setKatap(scan.nextInt());
+            
+    System.out.println("Kondisi Pintu   : ");
+    System.out.println("1. Bersih");
+    System.out.println("2. Kotor");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setKpintu(scan.nextInt());
+            
+    System.out.println("Kondisi Jendela : ");
+    System.out.println("1. Bersih");
+    System.out.println("2. Kotor");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setKjendela(scan.nextInt());
+        
     // kebersihan
+    System.out.print("masukkan jumlah sirkulasi:");
+    kenal.setSirkulasi(scan.nextInt());
+    
+    System.out.print("masukkan jumlah pencahayaan / lampu:");
+    kenal.setPencahayaan(scan.nextInt());
+    
+    System.out.print("masukkan intensitas kelembapan");
+    kenal.setLembab(scan.nextInt());
+    
+    System.out.print("masukkan suhu ruangan:");
+    kenal.setSuhu(scan.nextInt());
+        
     // kenyamanan
+    System.out.print("Tingkat kebisingan : \n");
+    System.out.println("1. Bising");
+    System.out.println("2. Tidak bising");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setTkebisingan(scan.nextInt());
+            
+    System.out.println("Tingkat bau : ");
+    System.out.println("1. Bau");
+    System.out.println("2. Tidak Bau");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setTbau(scan.nextInt());
+        
+    System.out.println("Tingkat kebocoran :");
+    System.out.println("1. Bocor");
+    System.out.println("2. Tidak bocor");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setTkebocoran(scan.nextInt());
+        
+    System.out.println("Tingkat kerusakan :");
+    System.out.println("1. Rusak");
+    System.out.println("2. Baik");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setTkerusakan(scan.nextInt());
+        
+    System.out.println("Tingkat keausan :");
+    System.out.println("1. Aus");
+    System.out.println("2. Baik");
+    System.out.print("Masukkan Pilihan    : ");
+    kenal.setTkeausan(scan.nextInt());
+    
     // keamanan
+    System.out.print("cek kondisi bangunan:");
+    kenal.setKondisi(scan.next());
+        
+    System.out.print("apakah kunci ada:");
+    kenal.setKunci(scan.next());
+    System.out.print("apakah jendala aman:");
+    kenal.setJendela(scan.next());
+        
+    }
 }
