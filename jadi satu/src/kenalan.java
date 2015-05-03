08573f0e272f2b0ab5fef4b3c475a3df85b138a6
riyoari <priyo.ari.wibowo@gmail.com>
2015-05-03 09:25:01 +0700

    Initial commit

---
+++ jadi satu/src/kenalan.java
@@ -0,0 +1,10 @@
+public class kenalan {
+    // identitas ruang
+    String saya;
+    // kondisi ruang
+    // jumlah,kondisi dan posisi
+    // lingkungan
+    // kebersihan
+    // kenyamanan
+    // keamanan
+}
---
+++ jadi satu/src/kontrol.java
@@ -0,0 +1,9 @@
+public class kontrol {
+    // identitas ruang
+    // kondisi ruang
+    // jumlah,kondisi dan posisi
+    // lingkungan
+    // kebersihan
+    // kenyamanan
+    // keamanan
+}
