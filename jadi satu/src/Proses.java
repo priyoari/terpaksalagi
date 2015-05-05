9b9851f9bcaf4fe7c192ca743b4d614ed867b8d1
riyoari <priyo.ari.wibowo@gmail.com>
2015-05-05 10:06:28 +0700

    Initial commit

--- jadi satu/src/Proses.java
+++ jadi satu/src/Proses.java
@@ -36,8 +36,7 @@ public class Proses {
             System.out.println("Jumlah jendela tidak sesuai");
         }
     }
-    
-        
+           
     // jumlah,kondisi dan posisi
     boolean kelistrikan(){
         if(kenal.getKontak()>=4&&kenal.getKondisiKontak().equals("ya")&&kenal.getPosisiKontak().equals("ya"))
@@ -53,118 +52,156 @@ public class Proses {
             System.out.println("tidak Sesuai");
     }    
     
-        
-    if(kenal.getLampu()>=18&&kenal.getKondisiLampu().equals("ya")&&kenal.getPosisiLampu().equals("ya"))
-        System.out.println("Sesuai");
-    if(kenal.getLampu()<=18||kenal.getKondisiLampu().equals("tidak")||kenal.getPosisiLampu().equals("tidak"))
-        System.out.println("tidak Sesuai");
-        
-    if(kenal.getKipas()>=2&&kenal.getKondisiKipas().equals("ya")&&kenal.getPosisiKipas().equals("ya"))
-        System.out.println("Sesuai");
-    if(kenal.getKipas()<=2||kenal.getKondisiKipas().equals("tidak")||kenal.getPosisiKipas().equals("tidak"))
-        System.out.println("tidak Sesuai");
-        
-    if(kenal.getAC()>=1&&kenal.getKondisiAC().equals("ya")&&kenal.getPosisiAC().equals("ya"))
-        System.out.println("Sesuai");
-    if(kenal.getAC()<=1||kenal.getKondisiAC().equals("tidak")||kenal.getPosisiAC().equals("tidak"))
-        System.out.println("tidak Sesuai");
+    boolean lampu(){
+        if(kenal.getLampu()>=18&&kenal.getKondisiLampu().equals("ya")&&kenal.getPosisiLampu().equals("ya"))
+            System.out.println("Sesuai");
+        if(kenal.getLampu()<=18||kenal.getKondisiLampu().equals("tidak")||kenal.getPosisiLampu().equals("tidak"))
+            System.out.println("tidak Sesuai");
+    }    
     
-    if(kenal.getSSID()==1)
-        System.out.println("Sesuai");
-    if(kenal.getSSID()!=1)
-        System.out.println("tidak Sesuai");
+    boolean kipas(){
+        if(kenal.getKipas()>=2&&kenal.getKondisiKipas().equals("ya")&&kenal.getPosisiKipas().equals("ya"))
+            System.out.println("Sesuai");
+        if(kenal.getKipas()<=2||kenal.getKondisiKipas().equals("tidak")||kenal.getPosisiKipas().equals("tidak"))
+            System.out.println("tidak Sesuai");
+    }    
     
-    if(kenal.getCCTV()>=2&&kenal.getKondisiCCTV().equals("ya")&&kenal.getPosisiCCTV().equals("ya"))
-        System.out.println("Sesuai");
-    if(kenal.getCCTV()<=2||kenal.getKondisiCCTV().equals("tidak")||kenal.getPosisiCCTV().equals("tidak"))
-        System.out.println("tidak Sesuai");
+    boolean ac(){
+        if(kenal.getAC()>=1&&kenal.getKondisiAC().equals("ya")&&kenal.getPosisiAC().equals("ya"))
+            System.out.println("Sesuai");
+        if(kenal.getAC()<=1||kenal.getKondisiAC().equals("tidak")||kenal.getPosisiAC().equals("tidak"))
+            System.out.println("tidak Sesuai");
+    }    
     
+    boolean internet(){
+        if(kenal.getSSID()==1)
+            System.out.println("Sesuai");
+        if(kenal.getSSID()!=1)
+            System.out.println("tidak Sesuai");
+    }
+    
+    boolean cctv(){
+        if(kenal.getCCTV()>=2&&kenal.getKondisiCCTV().equals("ya")&&kenal.getPosisiCCTV().equals("ya"))
+            System.out.println("Sesuai");
+        if(kenal.getCCTV()<=2||kenal.getKondisiCCTV().equals("tidak")||kenal.getPosisiCCTV().equals("tidak"))
+            System.out.println("tidak Sesuai");
+    }
+        
     // lingkungan
-    if(kenal.getKlantai()==1)
-        System.out.println("Kondisi lantai bersih, Sesuai");
-    else
-        System.out.println("Kondisi lantai kotor, Tidak sesuai");
+    boolean lantai(){
+        if(kenal.getKlantai()==1)
+            System.out.println("Kondisi lantai bersih, Sesuai");
+        else
+            System.out.println("Kondisi lantai kotor, Tidak sesuai");
+    }
     
-    if(kenal.getKdinding()==1)
-        System.out.println("Kondisi dinding bersih, Sesuai");
-    else
-        System.out.println("Kondisi dinding kotor, Tidak sesuai");
+    boolean dinding(){
+        if(kenal.getKdinding()==1)
+            System.out.println("Kondisi dinding bersih, Sesuai");
+        else
+            System.out.println("Kondisi dinding kotor, Tidak sesuai");
+    }
     
-    if(kenal.getKatap()==1)
-        System.out.println("Kondisi atap bersih, Sesuai");
-    else
-        System.out.println("Kondisi atap kotor, Tidak sesuai");
+    boolean atap(){
+        if(kenal.getKatap()==1)
+            System.out.println("Kondisi atap bersih, Sesuai");
+        else
+            System.out.println("Kondisi atap kotor, Tidak sesuai");
+    }
     
-    if(kenal.getKpintu()==1)
-        System.out.println("Kondisi pintu bersih, Sesuai");
-    else
-        System.out.println("Kondisi pintu kotor, Tidak sesuai");
+    boolean pintu(){
+        if(kenal.getKpintu()==1)
+            System.out.println("Kondisi pintu bersih, Sesuai");
+        else
+            System.out.println("Kondisi pintu kotor, Tidak sesuai");
+    }
     
-    if(kenal.getKjendela()==1)
-        System.out.println("Kondisi jendela bersih, Sesuai");
-    else
-        System.out.println("Kondisi jendela kotor, Tidak sesuai");
-    
+    boolean jendela(){
+        if(kenal.getKjendela()==1)
+            System.out.println("Kondisi jendela bersih, Sesuai");
+        else
+            System.out.println("Kondisi jendela kotor, Tidak sesuai");
+    }
+        
     // kebersihan
-    if(kenal.getSirkulasi()>4){
-        System.out.println("sirkulasi oke!!");
-    }else if(kenal.getSirkulasi()==4){
-        System.out.println("sirkulasi lumayan");
-    }else{
-        System.out.println("sirkulasi buruk");
+    boolean udara(){
+        if(kenal.getSirkulasi()>4){
+            System.out.println("sirkulasi oke!!");
+        }else if(kenal.getSirkulasi()==4){
+            System.out.println("sirkulasi lumayan");
+        }else{
+            System.out.println("sirkulasi buruk");
+        }
     }
     
-    if(kenal.getPencahayaan()>=250){
-        System.out.println("cahaya kurang");
-    }else if(kenal.getPencahayaan()<=350){
-        System.out.println("cahaya sesuai");
-    }else{
-        System.out.println("cahaya oke");
+    boolean cahaya(){
+        if(kenal.getPencahayaan()>=250){
+            System.out.println("cahaya kurang");
+        }else if(kenal.getPencahayaan()<=350){
+            System.out.println("cahaya sesuai");
+        }else{
+            System.out.println("cahaya oke");
+        }
     }
     
-    if(kenal.getPencahayaan()>=70){
-        System.out.println("kelembapan parah");
-    }else if(kenal.getPencahayaan()==75){
-        System.out.println("kelembapan rata-rata");
-    }else{
-        System.out.println("ruangan sesuai");
+    boolean lembab(){
+        if(kenal.getLembab()>=70){
+            System.out.println("kelembapan parah");
+        }else if(kenal.getLembab()==75){
+            System.out.println("kelembapan rata-rata");
+        }else{
+            System.out.println("ruangan sesuai");
+        }
     }
     
-    if(kenal.getSuhu()<=25){
-        System.out.println("sangat lembab");
-    }else if(kenal.getSuhu()<=35){
-        System.out.println("suhu sesuai");
-    }else{
-        System.out.println("suhu terlalu panas");
+    boolean suhu(){
+        if(kenal.getSuhu()<=25){
+            System.out.println("sangat lembab");
+        }else if(kenal.getSuhu()<=35){
+            System.out.println("suhu sesuai");
+        }else{
+            System.out.println("suhu terlalu panas");
+        }
     }
-    
+        
     // kenyamanan
-    if(kenal.getTkebisingan()==1)
-        System.out.println("Tidak sesuai");
-    else
-        System.out.println("Sesuai");
+    boolean bising(){
+        if(kenal.getTkebisingan()==1)
+            System.out.println("Tidak sesuai");
+        else
+            System.out.println("Sesuai");
+    }
     
-    if(kenal.getTbau()==1)
-        System.out.println("Tidak sesuai");
-    else
-        System.out.println("Sesuai");
+    boolean bau(){
+        if(kenal.getTbau()==1)
+            System.out.println("Tidak sesuai");
+        else
+            System.out.println("Sesuai");
+    }
     
-    if(kenal.getTkebocoran()==1)
-        System.out.println("Tidak sesuai");
-    else
-        System.out.println("Sesuai");
+    boolean bocor(){
+        if(kenal.getTkebocoran()==1)
+            System.out.println("Tidak sesuai");
+        else
+            System.out.println("Sesuai");
+    }
     
-    if(kenal.getTkerusakan()==1)
-        System.out.println("Tidak sesuai");
-    else
-        System.out.println("Sesuai");
+    boolean rusak(){
+        if(kenal.getTkerusakan()==1)
+            System.out.println("Tidak sesuai");
+        else
+            System.out.println("Sesuai");
+    }
     
-    if(kenal.getTkeausan()==1)
-        System.out.println("Tidak sesuai");
-    else
-        System.out.println("Sesuai");
-    
+    boolean aus(){
+        if(kenal.getTkeausan()==1)
+            System.out.println("Tidak sesuai");
+        else
+            System.out.println("Sesuai");
+    }
+        
     // keamanan
+    
     if(kenal.getKondisi().equals("oke"))
         System.out.println("bangunan oke sip");
     else
